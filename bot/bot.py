31036e902d4a7e956999f00b5f5163650b385ae3
Abderamane Oualili <a.oualili@esi-sba.dz>
2022-12-09 18:48:59 +0100

    fix lastname

--- bot/bot.py
+++ bot/bot.py
@@ -19,7 +19,7 @@ async def chabiba(update: Update, context: ContextTypes.DEFAULT_TYPE):
     admins = await context.bot.get_chat_administrators(chat_id)
 
     #[await context.bot.send_message(chat_id, text= "["+" ](tg://user?id="+str(x.user.id)+")", parse_mode="Markdown") for x in admins]
-    await context.bot.send_message(chat_id, text=' '.join([ "["+str(x.user.username or x.user.first_name+" "+x.user.last_name)+"](tg://user?id="+str(x.user.id)+")" for x in admins ]), parse_mode="Markdown")
+    await context.bot.send_message(chat_id, text=' '.join([ "["+str(x.user.username or x.user.first_name+(" "+x.user.last_name if x.user.last_name else  "") )+"](tg://user?id="+str(x.user.id)+")" for x in admins ]), parse_mode="Markdown")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
