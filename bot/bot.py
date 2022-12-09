1cd0edc3e8f76f8e179238b053d69a757cc9e11c
Abderamane Oualili <a.oualili@esi-sba.dz>
2022-12-09 18:06:58 +0100

    fix token

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
-from dotenv import dotenv_values
-TOKEN = dotenv_values(".env")['TOKEN']
+from os import getenv
+TOKEN = getenv("TOKEN")
 
 import logging
 
@@ -18,8 +18,8 @@ async def chabiba(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     admins = await context.bot.get_chat_administrators(chat_id)
 
-    [await context.bot.send_message(chat_id, text= "["+" ](tg://user?id="+str(x.user.id)+")", parse_mode="Markdown") for x in admins]
-
+    #[await context.bot.send_message(chat_id, text= "["+" ](tg://user?id="+str(x.user.id)+")", parse_mode="Markdown") for x in admins]
+    await context.bot.send_message(chat_id, text=' '.join([ "["+str(x.user.username or x.user.first_name+" "+x.user.last_name)+"](tg://user?id="+str(x.user.id)+")" for x in admins ]), parse_mode="Markdown")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
